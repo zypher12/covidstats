0c5265a35e9fbb3893e69cff5de121ec035b138d
zypher12 <34564683+zypher12@users.noreply.github.com>
2020-05-09 19:28:49 +0530

    code updated.
    Uses drop down to select state. Also shows total cases state and district wise

--- covid.py
+++ covid.py
@@ -3,21 +3,24 @@ app = Flask(__name__)
 import json
 import urllib.request
 
+url = 'https://api.covid19india.org/v2/state_district_wise.json'
+data = urllib.request.urlopen(url).read().decode()
+obj = json.loads(data)
+
 @app.route('/state')
 def student():
-   return render_template('state.html')
+   return render_template('state.html', state = obj , length = len(obj))
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form['nm']
-      url = 'https://api.covid19india.org/v2/state_district_wise.json'
-      data = urllib.request.urlopen(url).read().decode()
-      obj = json.loads(data)
+      sum = 0
       for i in range(0,len(obj)):
           if(obj[i]['state'] == result):
-              print(len(obj[i]['districtData']))
-              return render_template("result.html",result = obj[i], length = len(obj[i]['districtData']))
+                for j in range(0,len(obj[i]['districtData'])):
+                    sum += obj[i]['districtData'][j]['confirmed']
+                return render_template("result.html",result = obj[i], length = len(obj[i]['districtData']), total = sum)
 
 if __name__ == '__main__':
    app.run(debug = True,port=8080)
